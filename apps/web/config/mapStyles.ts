2c8827521c45c35ee006d2207794cd480f983574
ptrn23 <pnecasio23@gmail.com>
2026-02-04 21:14:35 +0800

    feat: update map palette to be darker

--- apps/web/config/mapStyles.ts
+++ apps/web/config/mapStyles.ts
@@ -1,347 +1,76 @@
 export const midnightTheme = [
     {
-        "featureType": "all",
+        "elementType": "geometry",
+        "stylers": [
+            { "color": "#050505" }
+        ]
+    },
+    {
         "elementType": "labels.text.fill",
         "stylers": [
-            {
-                "saturation": 36
-            },
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 40
-            }
+            { "color": "#525252" }
         ]
     },
     {
-        "featureType": "all",
         "elementType": "labels.text.stroke",
         "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 16
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 20
-            }
+            { "color": "#000000" }
         ]
     },
     {
         "featureType": "administrative",
         "elementType": "geometry.stroke",
         "stylers": [
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 17
-            },
-            {
-                "weight": 1.2
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.country",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": "0"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.country",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": "13"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 20
-            }
+            { "color": "#111111" },
+            { "weight": 1.2 }
         ]
     },
     {
         "featureType": "poi",
-        "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 21
-            }
+            { "visibility": "off" }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            { "visibility": "off" }
         ]
     },
     {
         "featureType": "road",
-        "elementType": "all",
+        "elementType": "geometry",
         "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "saturation": "-100"
-            },
-            {
-                "lightness": "-20"
-            },
-            {
-                "invert_lightness": true
-            }
+            { "color": "#121212" }
         ]
     },
     {
         "featureType": "road",
         "elementType": "geometry.stroke",
         "stylers": [
-            {
-                "color": "#bebebe"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": "-47"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "lightness": "-33"
-            },
-            {
-                "weight": "0.52"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
+            { "color": "#1f1f1f" },
+            { "weight": 0.5 }
         ]
     },
     {
         "featureType": "road.highway",
         "elementType": "geometry",
         "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#b5b5b5"
-            },
-            {
-                "saturation": "-1"
-            },
-            {
-                "gamma": "0.00"
-            },
-            {
-                "weight": "2.22"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "lightness": "0"
-            },
-            {
-                "visibility": "on"
-            },
-            {
-                "weight": "2.8"
-            },
-            {
-                "color": "#585858"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#909090"
-            },
-            {
-                "lightness": "2"
-            },
-            {
-                "weight": "0.2"
-            },
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "lightness": "16"
-            },
-            {
-                "color": "#595959"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "lightness": "-63"
-            },
-            {
-                "weight": "1"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 18
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": "10"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": "28"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "weight": "0.1"
-            },
-            {
-                "lightness": "-96"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 16
-            }
+            { "color": "#1a1a1a" }
         ]
     },
     {
         "featureType": "transit",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#000000"
-            },
-            {
-                "lightness": 19
-            }
+            { "color": "#000000" }
         ]
     },
     {
         "featureType": "water",
         "elementType": "geometry",
         "stylers": [
-            {
-                "color": "#12161a"
-            },
-            {
-                "lightness": 17
-            }
+            { "color": "#020406" }
         ]
     }
 ];
