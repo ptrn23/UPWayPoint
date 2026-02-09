ccca5e7ea74168a01fbf325873a1b22a22bca500
jpbuizon <jpbuizon@up.edu.ph>
2026-02-09 11:49:52 +0800

    auth setup

---
+++ packages/auth/src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@repo/db"; // your drizzle instance
+
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "pg", // or "mysql", "sqlite"
+    }),
+  emailAndPassword: { 
+    enabled: false, 
+  }, 
+  socialProviders: { 
+    google: { 
+      clientId: process.env.GOOGLE_CLIENT_ID as string, 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+    }, 
+  }, 
+});
