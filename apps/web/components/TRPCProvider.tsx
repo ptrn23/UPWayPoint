26df2300390d7498236cfcbafb3ef0f4a9803885
jpbuizon <jpbuizon@up.edu.ph>
2026-02-13 14:34:57 +0800

    feat: trpc client setup

---
+++ apps/web/components/TRPCProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { useState } from "react";
+import { trpc } from "@/lib/trpc";
+export function TRPCProvider({ children }: { children: React.ReactNode }) {
+	const [queryClient] = useState(() => new QueryClient());
+	const [trpcClient] = useState(() =>
+		trpc.createClient({
+			links: [
+				httpBatchLink({
+					url: "/api/trpc",
+					// You can pass any HTTP headers you wish here
+					// async headers() {
+					// 	return {
+					// 		authorization: getAuthCookie(),
+					// 	};
+					// },
+				}),
+			],
+		}),
+	);
+
+	return (
+		<trpc.Provider client={trpcClient} queryClient={queryClient}>
+			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		</trpc.Provider>
+	);
+}
