bd78609a0c75914f6c98792444b90ea41042f30b
jpbuizon <jpbuizon@up.edu.ph>
2026-02-13 14:33:49 +0800

    feat: export types and update package dependencies

--- packages/db/src/db/database.ts
+++ packages/db/src/db/database.ts
@@ -9,3 +9,4 @@ let connectionString = process.env.DATABASE_URL!;
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client, { schema });
+export type Database = typeof db;
