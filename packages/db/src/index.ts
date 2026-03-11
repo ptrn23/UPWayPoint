60b84d6c440a869387e3c54077419f60b0a332eb
jpbuizon <jpbuizon@up.edu.ph>
2026-03-11 17:50:09 +0800

    feat: forgot to add to repositories

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,9 +1,16 @@
 import { db } from "./db/database";
-import { makeUserRepository, makePinRepository } from "./repositories";
+import {
+	makeUserRepository,
+	makePinRepository,
+	makeTagRepository,
+} from "./repositories";
+import { makePinTagsRepository } from "./repositories/pinTags.repository";
 
 export const repositories = {
 	user: makeUserRepository(db),
 	pin: makePinRepository(db),
+	pinTags: makePinTagsRepository(db),
+	tag: makeTagRepository(db),
 };
 
 export * from "./db/database";
