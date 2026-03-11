c2db78e38e9228231abe9f445cd481841ea5bed9
jpbuizon <jpbuizon@up.edu.ph>
2026-03-11 22:37:47 +0800

    feat: comment router

---
+++ packages/api/src/routers/comment.router.ts
@@ -0,0 +1,24 @@
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import { router, userProcedure } from "../trpc";
+import z from "zod";
+
+export const commentRouter = router({
+	create: userProcedure
+		.input(
+			z.object({
+				message: z.string(),
+				pinId: z.string(),
+				parentId: z.string().optional(),
+			}),
+		)
+		.mutation(async ({ ctx, input }) => {
+			return ctx.services.comment.create({
+				...input,
+				ownerId: ctx.user.id,
+			});
+		}),
+});
+
+type CommentRouter = typeof commentRouter;
+export type CommentRouterOutputs = inferRouterOutputs<CommentRouter>;
+export type CommentRouterInputs = inferRouterInputs<CommentRouter>;
