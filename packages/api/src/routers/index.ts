40a78a85bc0a6b0f8b569a1924499726c2dfcb74
jpbuizon <jpbuizon@up.edu.ph>
2026-03-26 14:23:28 +0800

    feat: modifications connect router and service

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -3,12 +3,14 @@ import { userRouter } from "./user.router";
 import { pinRouter } from "./pins.router";
 import { tagRouter } from "./tag.router";
 import { commentRouter } from "./comment.router";
+import { modificationRouter } from "./modification.router";
 
 export const appRouter = router({
 	user: userRouter,
 	pin: pinRouter,
 	tag: tagRouter,
 	comment: commentRouter,
+	modification: modificationRouter,
 });
 
 export type AppRouter = typeof appRouter;
--- packages/api/src/services/index.ts
+++ packages/api/src/services/index.ts
@@ -3,6 +3,7 @@ import { makeUserService } from "./user.service";
 import { makePinService } from "./pins.service";
 import { makeTagService } from "./tag.service";
 import { makeCommentService } from "./comment.service";
+import { makeModificationService } from "./modification.service";
 
 const commentService = makeCommentService(repositories, db);
 
@@ -11,6 +12,7 @@ export const services = {
 	pin: makePinService(repositories, { comment: commentService }, db),
 	tag: makeTagService(repositories, db),
 	comment: commentService,
+	modification: makeModificationService(repositories, db),
 };
 
 export type Services = typeof services;
