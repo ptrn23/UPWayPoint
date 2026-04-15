50feb41e18601a9204971d5e2a06a335d160f285
jpbuizon <jpbuizon@up.edu.ph>
2026-04-16 01:22:26 +0800

    feat: make caller only need headers since we can fetch that anywhere

--- packages/api/src/caller.ts
+++ packages/api/src/caller.ts
@@ -1,6 +1,6 @@
 import "server-only";
 import { cache } from "react";
-import { createCallerFactory, createContext } from "./trpc";
+import { createCallerFactory, createContextFromHeaders } from "./trpc";
 import { appRouter, type AppRouter } from "./routers";
 
 const createCaller = createCallerFactory(appRouter);
@@ -8,8 +8,8 @@ const createCaller = createCallerFactory(appRouter);
 type ServerCaller = ReturnType<AppRouter["createCaller"]>;
 
 export const serverUserCaller = cache(
-  async (req: Request): Promise<ServerCaller> => {
-    const context = await createContext({ req });
-    return createCaller(context);
-  },
+	async (headers: Headers): Promise<ServerCaller> => {
+		const context = await createContextFromHeaders({ headers });
+		return createCaller(context);
+	},
 );
