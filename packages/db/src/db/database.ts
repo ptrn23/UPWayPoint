6e9f0a771bcf4cafe67c5da4afbc419e3ecf90e4
jpbuizon <jpbuizon@up.edu.ph>
2026-04-16 01:20:14 +0800

    feat: trying to use neon instead of postrgres to get out of 1101 hell

--- packages/db/src/db/database.ts
+++ packages/db/src/db/database.ts
@@ -1,16 +1,16 @@
 import "dotenv/config";
 
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
+import { neon } from "@neondatabase/serverless";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set!");
+	throw new Error("DATABASE_URL is not set!");
 }
 
 const connectionString = process.env.DATABASE_URL;
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, { prepare: false });
+const client = neon(connectionString);
 export const db = drizzle(client, { schema });
 export type Database = typeof db;
